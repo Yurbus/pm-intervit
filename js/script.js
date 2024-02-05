2c117f2edab61e590e1b8dc4c94f6460904614cd
Yurbus <frankins636@gmail.com>
2024-02-05 16:54:06 +0200

    qwqwqw

--- js/script.js
+++ js/script.js
@@ -25,9 +25,50 @@ const isMobile = {
             isMobile.Windows());
     }
 };
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+	
+			let menuArrows = document.querySelectorAll('.header__arrow');
+			if (menuArrows.length > 0) {
+				for (let index = 0; index < menuArrows.length; index++) {
+					const menuArrow = menuArrows[index];
+					menuArrow.addEventListener("click", function (e) {
+						menuArrow.parentElement.classList.toggle('_active');
+					});
+				}
+			}
+
+
+
+
+} else {
+    document.body.classList.add('_pc');
+}
+
+
+
+//-------------------------------------------------------------------------------------------------
+// открыть под меню
+document.addEventListener('DOMContentLoaded', function () {
+	let carrentMenuLink = document.querySelector('.carrent-menu__link');
+
+	carrentMenuLink.addEventListener('click', function () {
+		// Переключаем класс "clicked" для изменения стилей
+		this.classList.toggle('clicked');
+	});
+});
+
+//-------------------------------------------------------------------------------------------------
+// Animation
+// AOS.init({
+//     duration: 1200,
+// })
+
+//-------------------------------------------------------------------------------------------------
 // Меню бурнер
 const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.header__mobile-menu');
+const menuBody = document.querySelector('.header__menu');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
 		document.body.classList.toggle('_lock');
@@ -36,65 +77,126 @@ if (iconMenu) {
 	});
 }
 
+//-------------------------------------------------------------------------------------------------
+// добавить класс при скроле страницы
+// let scrollpos = window.scrollY
+
+// const header = document.querySelector("header")
+// const scrollChange = 1
+
+// const add_class_on_scroll = () => header.classList.add("bg-head")
+// const remove_class_on_scroll = () => header.classList.remove("bg-head")
+
+// window.addEventListener('scroll', function() { 
+//     scrollpos = window.scrollY;
+
+//     if (scrollpos >= scrollChange) { 
+//         add_class_on_scroll() 
+//     }
+//     else { 
+//         remove_class_on_scroll() 
+//     }
+
+// })
+
+//-------------------------------------------------------------------------------------------------
+// Маска на телефон 
+// jQuery(document).ready(function($) {
+//     $("#phone_mask").mask("+41 (99)-999-99-99",{placeholder:" "});
+// });
+
+
+//-------------------------------------------------------------------------------------------------
+// Прокрутка чисел на странице 
+// function animateCount(element, targetValue, duration) {
+//     $({ count: parseInt(element.text()) }).animate({ count: targetValue }, {
+//         duration: duration,
+//         step: function () {
+//             element.text(Math.floor(this.count));
+//         },
+//         complete: function () {
+//             element.text(targetValue);
+//         }
+//     });
+// }
+// function startCountAnimation() {
+//     animateCount($('#years'), 22, 2000); // Adjust duration as needed
+//     animateCount($('#partners'), 15, 2000);
+//     animateCount($('#countries'), 14, 2000);
+// }
+// // Trigger the counting animation when the document is ready
+// $(document).ready(function () {
+//     startCountAnimation();
+// });
+//-------------------------------------------------------------------------------------------------
+// скрол на верх
+// jQuery(document).ready(function() {
+//     var btn = $('.up_btn');  
+//         btn.on('click', function(e) {
+//             e.preventDefault();
+//             $('html, body').animate({scrollTop:0}, '300');
+//         });
+//     });
+
+//-------------------------------------------------------------------------------------------------
 // Прокрутка при клике
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if(menuLinks.length > 0) {
-	menuLinks.forEach(menuLink => {
-		menuLink.addEventListener("click", onMenuLinkClick);
-	});
+// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+// if(menuLinks.length > 0) {
+// 	menuLinks.forEach(menuLink => {
+// 		menuLink.addEventListener("click", onMenuLinkClick);
+// 	});
 
-	function onMenuLinkClick(e) {
-		const menuLink = e.target;
-		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+// 	function onMenuLinkClick(e) {
+// 		const menuLink = e.target;
+// 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+// 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 		
-			if (iconMenu.classList.contains('_active')) {
-				document.body.classList.remove('_lock');
-				iconMenu.classList.remove('_active');
-				menuBody.classList.remove('_active');
-			}
+// 			if (iconMenu.classList.contains('_active')) {
+// 				document.body.classList.remove('_lock');
+// 				iconMenu.classList.remove('_active');
+// 				menuBody.classList.remove('_active');
+// 			}
 
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: "smooth"
-			});
-			e.preventDefault();
-		}
-	}
-}
+// 			window.scrollTo({
+// 				top: gotoBlockValue,
+// 				behavior: "smooth"
+// 			});
+// 			e.preventDefault();
+// 		}
+// 	}
+// }
 
-
+//-------------------------------------------------------------------------------------------------
 //прикрепление файла в форме
+// const formImage = document.getElementById('formImage');
+// const formPreview = document.getElementById('formPreview');
 
-const formImage = document.getElementById('formImage');
-const formPreview = document.getElementById('formPreview');
+// formImage.addEventListener('change', () => {
+// 	uploadFile(formImage.files[0]);
+// });
 
-formImage.addEventListener('change', () => {
-	uploadFile(formImage.files[0]);
-});
+// function uploadFile(file) {
+// 	if (!['image/jpeg', 'image/png', 'image/gif', 'image/pdf'].includes(file.type)) {
+// 		alert('Разрешены только изображения!');
+// 		formImage.value ='';
+// 		return;
+// 	}
+// 	if (file.size > 2 * 1024 * 1024) {
+// 		alert('Файл должен быть менее 2 МБ.');
+// 		return;
+// 	}
+// 	var reader = new FileReader();
+// 	reader.onload = function (e) {
+// 		formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+// 	};
+// 	reader.onerror = function (e) {
+// 		alert('Ошибка');
+// 	};
+// 	reader.readAsDataURL(file);
+// }
 
-function uploadFile(file) {
-	if (!['image/jpeg', 'image/png', 'image/gif', 'image/pdf'].includes(file.type)) {
-		alert('Разрешены только изображения!');
-		formImage.value ='';
-		return;
-	}
-	if (file.size > 2 * 1024 * 1024) {
-		alert('Файл должен быть менее 2 МБ.');
-		return;
-	}
-	var reader = new FileReader();
-	reader.onload = function (e) {
-		formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-	};
-	reader.onerror = function (e) {
-		alert('Ошибка');
-	};
-	reader.readAsDataURL(file);
-}
-
-
+//-------------------------------------------------------------------------------------------------
 //style main-bg
 // function ibg(){
 // 	$.each($('.ibg'), function(index, val) {
@@ -105,15 +207,34 @@ function uploadFile(file) {
 // };
 // ibg();
 
+//-------------------------------------------------------------------------------------------------
+// скрывать и показывать пароль в инпуте
+// var button2 = document.getElementById("showEye2");
+// button2.onclick = showEye2;
 
+// var input2 = document.getElementById("show2");
+// var icon = document.getElementById("i");
 
+// function showEye2 () {
+// 	if(input2.getAttribute('type') == 'password') {
+// 		input2.removeAttribute('type');
+// 		input2.setAttribute('type', 'text')
+// 		button2.innerHTML='<i id="i" class="far fa-eye-slash">';
+// 	} else {
+// 		input2.removeAttribute('type');
+// 		input2.setAttribute('type', 'password');
+// 		button2.innerHTML='<i id="i" class="far fa-eye">';
+// 	}
+// }
+
+//-------------------------------------------------------------------------------------------------
 // $('#form').submit(function(){
 // 	$.ajax({
 // 		type:"POST",
 // 		url: "mail.php",
 // 		data: $(this).serialize()
 // 	}).done(function() {
-// 	alert("Спасибо что обратились ко мне!");
+// 	alert("Thank you for contacting us!");
 // 	setTimeout(function() {
 // 		$.magnificPopup.close();
 // 	}, 1000);
